17c683206576449c70eb2be214b0bb9ff60ae203
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-03 14:02:07 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	printf ("%s\n","roper grammar, but the outcome is a piece of art,");
+	return (0);
+}
