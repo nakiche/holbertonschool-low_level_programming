26a6eab3c807aaa84eff1d4a42ec307ef50be67f
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-04 15:52:58 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	write (1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
+	return (1);
+}
