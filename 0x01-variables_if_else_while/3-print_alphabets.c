83045515d5d87c9eabceba164a99f706b0daf35d
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-04 13:20:07 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 97; a < 123; a++)
+	{
+		putchar(a);
+	}
+	for (b = 65; b < 91; b++)
+	{
+		putchar(b);
+	}
+	putchar (012);
+	return (0);
+}
