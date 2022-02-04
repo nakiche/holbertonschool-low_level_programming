10c609bca1f203de4ba6969dcdce0d0a604db578
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-04 15:31:16 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+
+	}	
+	putchar (012);
+	return (0);
+}
