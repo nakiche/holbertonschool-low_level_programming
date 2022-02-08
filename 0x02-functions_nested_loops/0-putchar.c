d1a7383f0a68b9c35f4d7ce84086fc1ae90d18ee
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-08 14:26:44 -0800

    directory and task 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: 0 always
+ */
+
+#include "main.h"
+
+int main(void)
+{
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar (012);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This is  
+my README for the directory 0x02-functions_nested_loops
+what does the script?
