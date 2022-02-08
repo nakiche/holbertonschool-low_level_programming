b6422339b7c73e9fbe4853603f274c51e4e9ee04
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-08 14:45:07 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: 0 always
+ */
+
+#include "main.h"
+/*
+int main(void)
+{
+*/	
+
+void print_alphabet(void)
+{	
+	
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		putchar(a);
+	}
+	putchar (012);
+
+
+}
+/*	return (0);
+
+}*/
