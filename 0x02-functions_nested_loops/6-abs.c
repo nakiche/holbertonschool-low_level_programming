ed9dc5355c45a1ca22bc9c243b56567e6a98d402
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-08 20:07:28 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * _abs - This is a description
+ *@c: Description of parameter c
+ *
+ * Return: c
+ */
+
+#include "main.h"
+
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		c = c;
+	}
+	else
+	{
+		c = c * -1;
+	}
+	return (c);
+}
