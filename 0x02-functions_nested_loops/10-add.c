050772b5efbd4eb92f12940da046365a82ed1abb
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-09 09:15:38 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *add - This is a description
+ *@a: Description of parameter a
+ *@b: Description of parameter b
+ * Return: c
+ */
+
+
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+
+}
