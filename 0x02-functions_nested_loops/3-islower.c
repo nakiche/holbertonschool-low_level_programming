3bb806e9cae123c7b349bad6e418b9faa77433d8
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-08 18:44:23 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,23 +3,23 @@
 #include <time.h>
 #include <ctype.h>
 /**
- * main - This is a description
+ * _islower - This is a description
+ *@c: Description of parameter c
  *
- * Return: 0 always
+ * Return: c
  */
 
 #include "main.h"
 
 int _islower(int c)
 {
-	
-	if( c >= 97 && c <123)
+	if (c >= 97 && c < 123)
 	{
- 	 _putchar ('1');
-   	}
+		c = 1;
+	}
 	else
 	{
-   	 _putchar('0' );
-  	}
-	return (0);
+		c = 0;
+	}
+	return (c);
 }
