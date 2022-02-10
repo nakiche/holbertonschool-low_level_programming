ee0d1699a0d8e502007fc2e377819a5a79e166c5
Thomas Naquiche <tommynaquiche@hotmail.com>
2022-02-09 17:41:46 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,8 +6,7 @@
 
 /**
  *jack_bauer - This is a description
- *@a: Description of parameter a
- *@b: Description of parameter b
+ *
  * Return: 0
  */
 
@@ -18,19 +17,16 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-	for (h = 0;h < 24;h++)
+	for (h = 0; h < 24; h++)
 	{
-		for (m = 0;m < 60;m++)
+		for (m = 0; m < 60; m++)
 		{
-			_putchar((h/10) + '0');		
-			_putchar((h%10) + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar((m/10) + '0');
-			_putchar((m%10) + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
-	}	
-
-	
-
+	}
 }
